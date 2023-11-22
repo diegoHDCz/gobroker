c7ae44444dd570e06afb16ae1bd13ece9b6fb097
Diego Czajka <diego.czajka@gmail.com>
2023-11-21 23:47:25 -0300

    feat order

--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module github.com/diegoHDCz/gobroker
 
 go 1.20
 
-require github.com/stretchr/testify v1.8.4
+require (
+	github.com/mattn/go-sqlite3 v1.14.18
+	github.com/stretchr/testify v1.8.4
+)
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
--- main.go
+++ main.go
@@ -1,5 +1,30 @@
 package main
 
+import (
+	"database/sql"
+
+	"github.com/diegoHDCz/gobroker/internal/infra/database"
+	"github.com/diegoHDCz/gobroker/internal/usecase"
+	_ "github.com/mattn/go-sqlite3"
+)
+
 func main() {
-	println("Hello,World")
+	db, err := sql.Open("sqlite3", "db.sqlite3")
+	if err != nil {
+		panic(err)
+	}
+	orderRepository := database.NewOrderRepository(db)
+	uc := usecase.NewCalculateFinalPrice(orderRepository)
+
+	input := usecase.OrderInput{
+		ID:    "123",
+		Price: 10.0,
+		Tax:   1.0,
+	}
+
+	output, err := uc.Execute(input)
+	if err != nil {
+		panic(err)
+	}
+	println(output)
 }
